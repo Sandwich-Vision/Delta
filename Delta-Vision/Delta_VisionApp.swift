c8f8ae9d3088d4364fc654ca6e312bd72270246c
Andy Roth <andy.roth@ouraring.com>
2024-08-17 21:26:35 -0700

    Verify DeltaCore imports correctly in visionOS app

--- Delta-Vision/Delta_VisionApp.swift
+++ Delta-Vision/Delta_VisionApp.swift
@@ -6,10 +6,16 @@
 //  Copyright © 2024 Riley Testut. All rights reserved.
 //
 
+import DeltaCore
 import SwiftUI
 
 @main
 struct Delta_VisionApp: App {
+
+    init() {
+        debugPrint("registered cores: \(Delta.registeredCores)")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
